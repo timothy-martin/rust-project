7321091fd7e81a41701772e8cd0e5e4aae63f25a
Tim Martin <timothy.martin@kcl.ac.uk>
2020-11-02 16:08:05 -0500

    lexer_kind project attempts to pass new LexerKind enum to LexerBuilder constructor

--- lexer_type/src/main.rs
+++ lexer_type/src/main.rs
@@ -1,18 +1,6 @@
 use lrlex::{LexerBuilder, LexerKind};
 
-// #[derive(Debug)]
-// pub enum LexerKind {
-//     LRNonStreamingLexer,
-//     Lex,
-//     Flex,
-// }
-
 fn main() {
-
-    let lex_type = LexerKind::Lex;
-    println!("{:?}", lex_type);
-    let flex_type = LexerKind::Flex;
-    println!("{:?}", flex_type);
 
     LexerBuilder::new()
         .lexerkind(LexerKind::Flex)
